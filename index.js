e357cf2a6b3a6d6c1e09be50dbf9251c0073c45a
ArturN <arturnista@gmail.com>
2016-05-25 09:40:54 -0300

    Server and views added

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-console.log("Hello world2");
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+
+app.set('views', __dirname + '/views');
+app.engine('html', require('ejs').renderFile);
+
+app.get('/', function (req, res) {
+    res.render('index.html');
+});
+
+app.listen((process.env.PORT || 5000), function () {
+  console.log('Example app listening on port ' + (process.env.PORT || 5000) + '!');
+});
