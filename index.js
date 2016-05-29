9607c56512e036c15f26373175eb86b59adc6d2e
ArturN <arturnista@gmail.com>
2016-05-29 14:57:52 -0300

    About page and Images

--- index.js
+++ index.js
@@ -15,6 +15,15 @@ app.get('/', function (req, res) {
     res.render('index.html');
 });
 
+app.get('/about', function (req, res) {
+    res.render('about.html');
+});
+
+app.get('/images/:image_name', function (req, res) {
+    var imageName = req.params.image_name + ".png"
+    res.sendFile(__dirname + "/images/" + imageName);
+});
+
 app.post('/compiler', function (req, res) {
     compiler.compile(req.body.code, function(result){
         res.send(result);
